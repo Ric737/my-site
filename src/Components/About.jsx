3ad84de3e8d35efeb0c76bd86b29ba75e5002d65
Mohammed Nihal Chammayil <123383739+Ric737@users.noreply.github.com>
2023-09-21 10:17:39 +0000

    Completed editing steps with projectlist remaining

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,28 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "abstract waves image";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Cybersecurity student studying at Jain University, Bangalore. I enjoy application development as much as my enthusiasm I have for cybersecurity.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web developer",
+  "Network security",
+  "Diagnostic and troubleshooting",
+  "Cloud Developer",
+  "Power Platform Developer",
+  "Java Developer",
+  "Photography"
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am a person devoted to exploring every kind of experiences life has to offer. As the person who has complete confidence in his observation ability, I am quick to learn new things and adapt to new environments. I am a person who is always ready to take on new challenges and I am always looking for new opportunities to learn and grow.";
 
 const About = () => {
   return (
